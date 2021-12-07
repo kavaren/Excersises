c6ae95e0e6e4f2d06f7de75b9e5544e3d318867b
mateusz pienkowski <mateuszp2286@gmail.com>
2021-12-07 18:18:18 +0100

    Initial solution to TwoSum problem

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,18 @@
+﻿
+/// <summary>
+///  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+///  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+///  You can return the answer in any order.
+/// </summary>
+
+//Example data
+int[] list = { 15, 11, 7, 2 };
+int target = 18;
+
+
+int[] result = Solutions.OptimalSolution(list, target);
+
+int[] result2 = Solutions.BasicSolution(list, target);
+
+
+Console.WriteLine(result[0] + " " + result[1]);
---
+++ TwoSum/TwoSum/Solutions.cs
@@ -0,0 +1,53 @@
+﻿internal class Solutions
+{
+
+    public static int[] BasicSolution(int[] listOfNumbers, int targetValue)
+    {
+        int[] list = listOfNumbers;
+        int target = targetValue;
+        int[] result = new int[2];
+
+        for (int i = 0; i < list.Length; i++)
+        {
+            for (int j = 0; j < list.Length; j++)
+            {
+
+                if ((list[i] + list[j]) == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+
+        }
+
+        return result;
+    }
+    public static int[] OptimalSolution(int[] listOfNumbers, int targetValue)
+    {
+        int[] list = listOfNumbers;
+        int target = targetValue;
+        Dictionary<int, int> map = new();
+        int diff;
+        int[] result = new int[2];
+        int i = 0;
+
+        foreach (var item in list)
+        {
+            diff = target - item;
+            if (map.ContainsKey(diff))
+            {
+                result[0] = map[diff];
+
+                result[1] = i;
+            }
+            else
+            {
+                map[item] = i;
+            }
+            i++;
+        }
+        return result;
+    }
+}
